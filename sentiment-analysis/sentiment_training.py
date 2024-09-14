c562e220c151a15f8c306b77a1a1f60dca833158
isaac <isaaccha721@gmail.com>
2024-09-14 08:35:33 -0400

    created model

--- sentiment-analysis/sentiment_training.py
+++ sentiment-analysis/sentiment_training.py
@@ -1,10 +1,11 @@
-from keras.preprocessing import sequence
-import keras
-import tensorflow as tf
-import os
-import numpy as np
 import pandas as pd
+import tensorflow as tf
+import numpy as np
 import re
+from keras.preprocessing import sequence
+import sys
+
+sys.stdout.reconfigure(encoding='utf-8')
 
 VOCAB_SIZE = 25000
 MAXLEN = 20
@@ -12,9 +13,9 @@ BATCH_SIZE = 64
 SPLIT = 0.8
 
 encoding = {
-    'negative': [1,0,0],
-    'neutral': [0,1,0], 
-    'positive': [0,0,1]
+    'negative': [1, 0, 0],
+    'neutral': [0, 1, 0], 
+    'positive': [0, 0, 1]
 }
 
 def clean_text(text):
@@ -24,7 +25,7 @@ def clean_text(text):
 def label_encoding(labels):
     encoded_labels = [encoding[label] for label in labels]
     return np.array(encoded_labels, dtype=np.float32)
-        
+
 traindf = pd.read_csv("/Users/isaac/Documents/Coding Projects/htn-project/sentiment-analysis/train.csv", encoding='ISO-8859-1')
 testdf = pd.read_csv("/Users/isaac/Documents/Coding Projects/htn-project/sentiment-analysis/test.csv", encoding='ISO-8859-1')
 
@@ -41,11 +42,10 @@ test_data = testdf['text'].tolist()
 test_labels = label_encoding(testdf['sentiment'].tolist())
 
 vectorize_layer = tf.keras.layers.TextVectorization(max_tokens=VOCAB_SIZE, output_sequence_length=MAXLEN)
-
 vectorize_layer.adapt(train_data)
 
-train_data = vectorize_layer(train_data)
-test_data = vectorize_layer(test_data)
+train_data = np.array(vectorize_layer(train_data))
+test_data = np.array(vectorize_layer(test_data))
 
 model = tf.keras.Sequential([ 
     tf.keras.layers.Embedding(VOCAB_SIZE, 32), 
@@ -53,24 +53,34 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(3, activation="softmax")
 ])
 
-model.compile(loss="categorical_crossentropy",optimizer="adam",metrics=['acc'])
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['acc'])
 
-history = model.fit(train_data, train_labels, epochs=20, validation_split=0.2)
+history = model.fit(train_data, train_labels, epochs=10, validation_split=0.2)
+
+results = model.evaluate(test_data, test_labels)
+print(results)
 
 word_index = vectorize_layer.get_vocabulary()
 
 def encode_text(text):
     tokens = tf.strings.split(text)
-    tokens = [word.decode('utf-8') for word in tokens.numpy()]  
-    tokens = [word_index[word] if word in word_index else 0 for word in tokens]
-    return sequence.pad_sequences([tokens], MAXLEN)[0]
+    tokens = [word.decode('utf-8') for word in tokens.numpy()]
+    int_tokens = []
+    for word in tokens:
+        if word in word_index:
+            int_tokens.append(word_index.index(word))
+        else:
+            int_tokens.append(0)
+    return sequence.pad_sequences([np.array(int_tokens)], maxlen=MAXLEN)[0]
 
-reverse_word_index = {value: key for (key, value) in word_index.items()}
+def predict(text):
+    encoded_text = encode_text(text)
+    pred = np.zeros((1, MAXLEN))
+    pred[0] = encoded_text
+    result = model.predict(pred)
+    print(result[0])
 
-def decode_int(ints):
-    PAD = 0
-    text = ""
-    for num in ints:
-      if num != PAD:
-        text += reverse_word_index[num] + " "
-    return text[:-1]
+# Save the model
+model.save('my_model.h5')
+
+# model = tf.keras.models.load_model('my_model.h5')    
