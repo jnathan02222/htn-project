50f292e7495420d1409ccd4c833f0c440450fdf1
Nathan <test@gmail.com>
2024-09-14 16:09:49 -0400

    WS change

--- website/app/page.js
+++ website/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   const ws = useRef();
 
   useEffect(()=>{ 
-    ws.current = new WebSocket('ws://localhost:8081');
+    ws.current = new WebSocket('ws://localhost:8081/ws');
     ws.current.addEventListener("open", (event) => {
       console.log("Connected to server.");
     });
@@ -81,60 +81,40 @@ export default function Home() {
     }
   }
   return (
-<<<<<<< HEAD
-    <div className="min-h-screen w-full flex justify-center p-24">
-      <div className='max-w-[1000px] 	'>
-        <div className='font-bold	text-5xl mb-5 leading-tight	'>Get the inside scoop. Personalized stock insights from your favourite websites.</div>
-        <div className='text-gray-500 text-2xl'>MoneyMoves analyzes market sentiment from hundreds of articles to help you invest.</div>
-        <div className='text-gray-500 text-2xl mb-5'>It all starts from one link.</div>
-        <form onSubmit={submitForm} className="flex items-center"> 
-          <input
-            name="website"
-            onChange={(e) => setWebsite(e.target.value)}
-            value={website}
-            placeholder="Enter any website!"
-            className="text-xl rounded-md border-2 p-2 hover:border-indigo-200 w-full focus:border-indigo-200 focus:outline-none"
-          />
-          <button type="submit" className="text-xl ml-2 rounded-md bg-indigo-200 text-black py-2 px-4 hover:bg-indigo-300">Search</button>
-        </form>
-        
-        {
-          sentenceCount > 0 && 
-          <div>
-            <div className="mt-4 text-3xl flex gap-1 items-end">
-            <div className="text-4xl font-bold items-baseline">{Math.floor(sentenceCount)}</div>
-            <div>sentences parsed.</div>
-            </div>
-            <div className="mt-4 text-3xl flex gap-1 items-end">
-              <div className="text-4xl font-bold items-baseline">{Math.floor(articleCount)}</div>
-              <div>articles analyzed.</div>
-            </div>
-          </div>
-        }
-      </div>
-    </div>
-=======
     <>
       <NavBar></NavBar>
       <div className="min-h-screen w-full flex justify-center p-24">
-              {showForm ? (
-          <form onSubmit={submitForm} className="flex flex-col items-center"> 
+        
+        <div className='max-w-[1000px] mt-10'>
+          <div className='font-bold	text-5xl mb-5 leading-tight	'>Get the inside scoop. Personalized stock insights from your favourite websites.</div>
+          <div className='text-gray-500 text-2xl'>MoneyMoves analyzes market sentiment from hundreds of articles to help you invest.</div>
+          <div className='text-gray-500 text-2xl mb-5'>It all starts from one link.</div>
+          <form onSubmit={submitForm} className="flex items-center"> 
             <input
               name="website"
               onChange={(e) => setWebsite(e.target.value)}
               value={website}
-              placeholder="Enter a website!"
-              className="rounded-md border-2 p-2 hover:border-indigo-200 min-w-[500px] focus:border-indigo-200 focus:outline-none"
+              placeholder="Enter any website!"
+              className="text-xl rounded-md border-2 p-2 hover:border-indigo-200 w-full focus:border-indigo-200 focus:outline-none"
             />
-            <button type="submit" className="mt-2 rounded-md bg-indigo-200 text-black py-2 px-4 hover:bg-indigo-300">Search</button>
+            <button type="submit" className="text-xl ml-2 rounded-md bg-indigo-200 text-black py-2 px-4 hover:bg-indigo-300">Search</button>
           </form>
-        ) : (
-          <div className="mt-4 text-lg">
-            Number of links found: {linkCount}
-          </div>
-        )}
+          
+          {
+            sentenceCount > 0 && 
+            <div>
+              <div className="mt-4 text-3xl flex gap-1 items-end">
+              <div className="text-4xl font-bold items-baseline">{Math.floor(sentenceCount)}</div>
+              <div>sentences parsed.</div>
+              </div>
+              <div className="mt-4 text-3xl flex gap-1 items-end">
+                <div className="text-4xl font-bold items-baseline">{Math.floor(articleCount)}</div>
+                <div>articles analyzed.</div>
+              </div>
+            </div>
+          }
+        </div>
       </div>
     </>
->>>>>>> origin/navbar
   );
 }
